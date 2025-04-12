143fdbcd81600eed0df88f5fd66e5b016303d62f
tsakumin <erkasuper969@gmail.com>
2025-04-12 12:07:34 +0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -13,7 +13,7 @@ module.exports = {
         ];
 
      
-        const statuses = ['online', 'idle', 'dnd'];
+        const statuses = ['online', 'dnd'];
 
      
         let currentActivityIndex = 0;
